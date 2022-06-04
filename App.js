fe1eda12f5496c8f399f41d77a073f2e44a67412
Morena <onalepeloo@live.com>
2022-06-04 15:30:58 +0200

    completed reuseable components

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import { StatusBar } from 'expo-status-bar';
 
 //we need to import these components from react native in order to utilize them inside our jsx blocks
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
 
 //import our custom made components
 import Header from './src/components/Header';
 import Search from './src/components/Search';
+import CategoryItem from './src/components/CategoryItem';
 
  const App = () => {
   return (
@@ -17,6 +19,9 @@ import Search from './src/components/Search';
     <View style={styles.container}>
       <Header/>
       <Search />
+      <CategoryItem name="Burger" imageUrl={require("./src/assets/images/burger.png")}/>
+      <CategoryItem name="Pizza" imageUrl={require("./src/assets/images/pizza.png")}/>
+      <CategoryItem name="Dessert" imageUrl={require("./src/assets/images/cake.png")}/>
       <StatusBar style="auto"/>
     </View>
   );
